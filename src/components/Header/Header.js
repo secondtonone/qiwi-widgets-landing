64408f0908e8154d220d266a32851de1d4f43358
Fedorov Nikita <n.fedorov@qiwi.com>
2017-10-02 21:43:41 +0300

    Header text changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,9 +12,9 @@ export default class Header extends Component {
         return (<header class="header">
             <a href="/" class="header__logo"><img src={logo} alt="logo" width="140" height="61" /></a>
             <section class="header__call-to-action">
-                <h1 class="header__title">Есть страничка или сайт? Ты можешь помочь!</h1>
-                <p class="header__description">Размести платежную форму у себя на сайте и твори добро во благо</p>
-                <a href={`#${idWidgetsBlock}`} class="header__action">Разместить</a>
+                <h1 class="header__title">Привлеки своих пользователей, покупателей и читателей к добрым делам!</h1>
+                <p class="header__description">Размести платежную форму у себя на сайте и поддержи Фонд Константина Хабенского.</p>
+                <a href={`#${idWidgetsBlock}`} class="header__action">Разместить виджет</a>
             </section>
             <div class="header__illustration">
                 <img src={widgetPic} alt="widgets" width="480" height="720"/>
